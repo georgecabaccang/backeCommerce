46fe01349ddda5adc84faf5078a6ebd1c56fac3c
georgesycabaccang <georgesycabaccang@gmail.com>
2023-05-05 14:41:56 +0800

    Intial testing of getUserCart feature

--- src/controllers/cartControllers.ts
+++ src/controllers/cartControllers.ts
@@ -4,7 +4,14 @@ import { ICartModel } from "../types/CartModel";
 import Item from "../models/itemModel";
 import { IItemModel } from "../types/ItemModel";
 
-
+export const getUserCart = async (req: Request, res: Response) => {
+    try {
+        const user = req.authenticatedUser;
+        res.send(user);
+    } catch (error) {
+        if (error instanceof Error) return res.send(error.message);
+    }
+};
 
 export const addToCart = async (req: Request, res: Response) => {
     try {
--- src/routes/carts.ts
+++ src/routes/carts.ts
@@ -1,7 +1,7 @@
 import express from "express";
-// import { getUserCart } from "../controllers/cartControllers";
+import { getUserCart } from "../controllers/cartControllers";
 import { authToken } from "../security/authentication";
 
 const cartsRoutes = express.Router();
 
-// cartsRoutes.get("/cart", authToken, getUserCart);
+cartsRoutes.get("/cart", authToken, getUserCart);
