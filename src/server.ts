0d0395191400fd27c1c76ac4a6bdf62e76cf55a7
georgesycabaccang <georgesycabaccang@gmail.com>
2023-05-29 14:20:16 +0800

    Try change endpoints if will work

--- src/server.ts
+++ src/server.ts
@@ -25,10 +25,10 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb" }));
 app.use(cors());
 
-app.use("/.netlify/source/routes/user", userRoutes);
-app.use("/.netlify/source/routes/shop", productRoutes);
-app.use("/.netlify/source/routes/cart", cartsRoutes);
-app.use("/.netlify/source/routes/orders", ordersRoutes);
+app.use("/user", userRoutes);
+app.use("/shop", productRoutes);
+app.use("/cart", cartsRoutes);
+app.use("/orders", ordersRoutes);
 
 app.listen(8002, () => {
     console.log("Port 8002");
