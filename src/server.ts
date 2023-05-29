8c808f3807b15e10039009c116aaa165c8d1c372
georgesycabaccang <georgesycabaccang@gmail.com>
2023-05-29 18:54:22 +0800

    Deployed, routing now working.

--- src/server.ts
+++ src/server.ts
@@ -27,14 +27,14 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb" }));
 app.use(cors());
 
-const route = Router();
-route.get("/hello", (req, res) => res.send("Hello World!"));
-app.use("/server", route);
+// const route = Router();
+// route.get("/hello", (req, res) => res.send("Hello World!"));
+// app.use("/server", route);
 
-// app.use("/user", userRoutes);
-// app.use("/shop", productRoutes);
-// app.use("/cart", cartsRoutes);
-// app.use("/orders", ordersRoutes);
+app.use("/user", userRoutes);
+app.use("/shop", productRoutes);
+app.use("/cart", cartsRoutes);
+app.use("/orders", ordersRoutes);
 
 app.listen(8002, () => {
     console.log("Port 8002");
