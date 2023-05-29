0b9d55b9ae7493efac60617054608d1ea5e06b35
georgesycabaccang <georgesycabaccang@gmail.com>
2023-05-29 13:59:48 +0800

    Change routing in server.ts

--- src/server.ts
+++ src/server.ts
@@ -25,13 +25,13 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb" }));
 app.use(cors());
 
-app.use("/.netlify/functions/api/user", userRoutes);
-app.use("/.netlify/functions/api/shop", productRoutes);
-app.use("/.netlify/functions/api/cart", cartsRoutes);
-app.use("/.netlify/functions/api/orders", ordersRoutes);
+app.use("/.netlify/src/routes/user", userRoutes);
+app.use("/.netlify/src/routes/shop", productRoutes);
+app.use("/.netlify/src/routes/cart", cartsRoutes);
+app.use("/.netlify/src/routes/orders", ordersRoutes);
 
-app.listen(8002, () => {
-    console.log("Port 8002");
-});
+// app.listen(8002, () => {
+//     console.log("Port 8002");
+// });
 
 module.exports.handler = ServerlessHttp(app);
