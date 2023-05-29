f1f1f988a1923af18740a007a40a41ca21bc93f2
georgesycabaccang <georgesycabaccang@gmail.com>
2023-05-29 15:27:10 +0800

    Minor alterations

--- src/server.ts
+++ src/server.ts
@@ -22,15 +22,15 @@ db.on("error", (error: IDBTypes) => {
 db.once("open", () => {
     console.log("Connected to DB");
 });
-app.use(bodyParser);
+app.use(bodyParser.json());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb" }));
 app.use(cors());
 
-app.use("/user", userRoutes);
-app.use("/shop", productRoutes);
-app.use("/cart", cartsRoutes);
-app.use("/orders", ordersRoutes);
+app.use("/.netlify/functions/routes/user", userRoutes);
+app.use("/.netlify/functions/routes/shop", productRoutes);
+app.use("/.netlify/functions/routes/cart", cartsRoutes);
+app.use("/.netlify/functions/routes/orders", ordersRoutes);
 
 app.listen(8002, () => {
     console.log("Port 8002");
