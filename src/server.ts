a63bf6023fc62e1ec5b8c53d8734ad7f31c833ee
georgesycabaccang <georgesycabaccang@gmail.com>
2023-05-29 15:44:28 +0800

    Ano na?

--- src/server.ts
+++ src/server.ts
@@ -27,10 +27,10 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb" }));
 app.use(cors());
 
-app.use("/.netlify/functions/routes/user", userRoutes);
-app.use("/.netlify/functions/routes/shop", productRoutes);
-app.use("/.netlify/functions/routes/cart", cartsRoutes);
-app.use("/.netlify/functions/routes/orders", ordersRoutes);
+app.use("/user", userRoutes);
+app.use("/shop", productRoutes);
+app.use("/cart", cartsRoutes);
+app.use("/orders", ordersRoutes);
 
 app.listen(8002, () => {
     console.log("Port 8002");
