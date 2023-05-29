0be04baac18fc34fd90dead74c768846b8e528ad
georgesycabaccang <georgesycabaccang@gmail.com>
2023-05-29 14:38:03 +0800

    try normal  routing

--- src/server.ts
+++ src/server.ts
@@ -25,10 +25,10 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb" }));
 app.use(cors());
 
-app.use("/.netlify/source/api/user", userRoutes);
-app.use("/.netlify/source/api/shop", productRoutes);
-app.use("/.netlify/source/api/cart", cartsRoutes);
-app.use("/.netlify/source/api/orders", ordersRoutes);
+app.use("/user", userRoutes);
+app.use("/shop", productRoutes);
+app.use("/cart", cartsRoutes);
+app.use("/orders", ordersRoutes);
 
 app.listen(8002, () => {
     console.log("Port 8002");
