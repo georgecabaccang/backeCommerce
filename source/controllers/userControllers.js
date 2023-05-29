561f85f2834cdd0e9be6c9ee96ef04eec0781c95
georgesycabaccang <georgesycabaccang@gmail.com>
2023-05-29 14:17:38 +0800

    change response in login

--- source/controllers/userControllers.js
+++ source/controllers/userControllers.js
@@ -84,7 +84,8 @@ const login = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
             email: userCredentials.email,
         });
         if (!user) {
-            return res.sendStatus(404);
+            // return res.sendStatus(404);
+            return res.send('no such user');
         }
         // Check if passwords match
         const match = yield bcrypt_1.default.compare(userCredentials.password, user.password);
