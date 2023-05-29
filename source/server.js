6cd0cb15e1b1e0ecdfe174f16f16294d243dc8f6
georgesycabaccang <georgesycabaccang@gmail.com>
2023-05-29 13:44:17 +0800

    Try configure for auto build in netlify

--- source/server.js
+++ source/server.js
@@ -10,6 +10,7 @@ const products_1 = __importDefault(require("./routes/products"));
 const users_1 = __importDefault(require("./routes/users"));
 const carts_1 = __importDefault(require("./routes/carts"));
 const orders_1 = __importDefault(require("./routes/orders"));
+const serverless_http_1 = __importDefault(require("serverless-http"));
 const app = (0, express_1.default)();
 mongoose_1.default.connect("mongodb+srv://miniprojects:thenewpassword@projects.wpbsykb.mongodb.net/eCommerce?retryWrites=true&w=majority");
 const db = mongoose_1.default.connection;
@@ -22,10 +23,11 @@ db.once("open", () => {
 app.use(express_1.default.json({ limit: "50mb" }));
 app.use(express_1.default.urlencoded({ limit: "50mb" }));
 app.use((0, cors_1.default)());
-app.use("/user", users_1.default);
-app.use("/shop", products_1.default);
-app.use("/cart", carts_1.default);
-app.use("/orders", orders_1.default);
+app.use("/.netlify/functions/api/user", users_1.default);
+app.use("/.netlify/functions/api/shop", products_1.default);
+app.use("/.netlify/functions/api/cart", carts_1.default);
+app.use("/.netlify/functions/api/orders", orders_1.default);
 app.listen(8002, () => {
     console.log("Port 8002");
 });
+module.exports.handler = (0, serverless_http_1.default)(app);
--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,8 @@ import userRoutes from "./routes/users";
 import cartsRoutes from "./routes/carts";
 import ordersRoutes from "./routes/orders";
 
+import ServerlessHttp from "serverless-http";
+
 const app = express();
 mongoose.connect(
     "mongodb+srv://miniprojects:thenewpassword@projects.wpbsykb.mongodb.net/eCommerce?retryWrites=true&w=majority"
@@ -23,11 +25,13 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb" }));
 app.use(cors());
 
-app.use("/user", userRoutes);
-app.use("/shop", productRoutes);
-app.use("/cart", cartsRoutes);
-app.use("/orders", ordersRoutes);
+app.use("/.netlify/functions/api/user", userRoutes);
+app.use("/.netlify/functions/api/shop", productRoutes);
+app.use("/.netlify/functions/api/cart", cartsRoutes);
+app.use("/.netlify/functions/api/orders", ordersRoutes);
 
 app.listen(8002, () => {
     console.log("Port 8002");
 });
+
+module.exports.handler = ServerlessHttp(app);
